683301996ab9e8e71e704e7f28690a7e00c77cd0
BPattjoshi <bibaswan.pattjoshi478@gmail.com>
2020-07-18 22:17:24 +0530

    Initial commit > added gradle file and initial setup added

---
+++ app/src/main/java/com/bibaswan/techmtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bibaswan.techmtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
